91b13ae84c38069f65793f6d39532348d270dee4
Thomas Arrow <thomasarrow@gmail.com>
2017-06-20 10:55:31 +0100

    tighten urls

--- src/add_paper/urls.py
+++ src/add_paper/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url, include
 from add_paper import views
 
 urlpatterns = [
-    url(r'profile', views.profile, name='profile'),
-    url(r'accounts/login', views.login_oauth, name='login'),
-    url(r'oauth/', include('social_django.urls', namespace='social')),
-    url(r'add/(?P<item>[0-9]+)', views.addPMID),
-    url(r'pmid/add/(?P<item>[0-9]+)', views.add),
-    url(r'pmc/add/(?P<item>PMC[0-9]+)', views.addPMCID),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^accounts/login$', views.login_oauth, name='login'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^add/(?P<item>[0-9]+)$', views.addPMID),
+    url(r'^pmid/add/(?P<item>[0-9]+$)', views.add),
+    url(r'^pmc/add/(?P<item>PMC[0-9]+$)', views.addPMCID),
     url(r'', views.index),
 ]
